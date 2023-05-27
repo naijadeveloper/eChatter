ccfa43f2bbdf8adfabebbe26628faa48f5956dba
Enoch <mmejuenoch@gmail.com>
2023-05-27 15:33:09 +0100

    Part 2 of writing the logic for the otp. Changed previous logic. This new logic works perfectly in the way I want it to. And the otp as a self-sufficient component is reusable.

---
+++ src/components/OtpInput.tsx
@@ -0,0 +1,136 @@
+import { useMemo, useState } from "react";
+import { RE_DIGIT } from "@/utilities/digits_only";
+
+type Props = {
+  value: string;
+  valueLength: number;
+  onChange: (value: string) => void;
+};
+
+//////////////////////////////////////////////////////////////////////////////
+export default function OtpInput({ value, valueLength, onChange }: Props) {
+  const [error, setError] = useState(false);
+
+  const valueItems = useMemo(() => {
+    // Construct array from value
+    // Array should have length equal to valueLength
+    // items in array should pass a regular expression test
+    const valueArry = value.split("");
+    const items: Array<string> = [];
+
+    for (let i = 0; i < valueLength; i++) {
+      const char = valueArry[i];
+
+      if (RE_DIGIT.test(char)) items.push(char);
+      else items.push("");
+    }
+
+    return items;
+  }, [value, valueLength]); // end of memo
+
+  ///////////////////////////////////////
+  function focusToNextElem(target: HTMLInputElement) {
+    const nextElemSib = target.nextElementSibling as HTMLInputElement | null;
+    if (nextElemSib) {
+      nextElemSib.focus();
+    }
+  }
+
+  function focusToPrevElem(target: HTMLInputElement) {
+    const prevElemSib =
+      target.previousElementSibling as HTMLInputElement | null;
+    if (prevElemSib) {
+      prevElemSib.focus();
+    }
+  }
+
+  function handleInputOnChange(
+    { target }: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) {
+    let targetValue = target.value.trim();
+    const isTargetValueDigit = RE_DIGIT.test(targetValue);
+    // If not a digit, do nothing
+    if (!isTargetValueDigit && targetValue !== "") return;
+    // else
+    // redefine targeValue if its empty string
+    targetValue = isTargetValueDigit ? targetValue : " ";
+
+    if (targetValue.length === 1) {
+      setError(false);
+
+      const newValue =
+        value.substring(0, index) + targetValue + value.substring(index + 1);
+      // Set new value
+      onChange(newValue);
+      // No need to focus to next input, when target value is empty string i.e when we are deleting
+      if (!isTargetValueDigit) return;
+      // change the focus of the inputs
+      focusToNextElem(target);
+    } else {
+      // handle pasting of value more than 1 digits
+      if (targetValue.length !== valueLength) {
+        setError(true);
+      } else if (targetValue.length === valueLength) {
+        setError(false);
+
+        onChange(targetValue);
+
+        target.blur();
+      }
+    }
+  }
+
+  ///////////////////////////////////////
+  function handleOnKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    const target = e.target as HTMLInputElement;
+
+    // handle ArrowRight, ArrowLeft, ArrowUp ArrowDown
+    if (e.key === "ArrowRight" || e.key === "ArrowDown") {
+      e.preventDefault();
+      return focusToNextElem(target);
+    }
+
+    if (e.key === "ArrowLeft" || e.key === "ArrowUp") {
+      e.preventDefault();
+      return focusToPrevElem(target);
+    }
+    // Keep the selection range position
+    target.setSelectionRange(0, target.value.length);
+    // If e.key is not backspace or target.value is not empty, do nothing
+    if (e.key !== "Backspace" || target.value !== "") return;
+    // change the focus of the inputs
+    focusToPrevElem(target);
+  }
+
+  ///////////////////////////////////////
+  function handleOnFocus({ target }: React.FocusEvent<HTMLInputElement>) {
+    target.setSelectionRange(0, target.value.length);
+  }
+
+  //////////////////////////////////////
+  return (
+    <div className="mt-8 flex w-full flex-wrap items-center justify-center gap-3">
+      {valueItems.map((digit, index) => (
+        <input
+          key={index}
+          type="text"
+          inputMode="numeric"
+          autoComplete="one-time-code"
+          pattern="\d{1}"
+          maxLength={valueLength}
+          value={digit}
+          onChange={(e) => handleInputOnChange(e, index)}
+          onKeyDown={handleOnKeyDown}
+          onFocus={handleOnFocus}
+          className="h-[60px] w-[60px] rounded-md bg-gray-100 text-center text-4xl font-semibold text-gray-800 outline-none dark:bg-gray-800 dark:text-gray-100"
+        />
+      ))}
+      {error && (
+        <span className="mt-5 flex w-full items-center justify-center text-sm text-red-700">
+          What you pasted isn't {valueLength} digits long
+        </span>
+      )}
+    </div>
+  );
+}
--- src/pages/account/otp.tsx
+++ src/pages/account/otp.tsx
@@ -1,38 +1,22 @@
 import Link from "next/link";
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useMemo } from "react";
 
 import ThemeSwitch from "@/components/ThemeSwitch";
-
-let currentOtpIndex: number = 0;
+import OtpInput from "@/components/OtpInput";
 
 export default function Otp() {
-  const [otp, setOtp] = useState<string[]>(new Array(8).fill(""));
-  const [activeOtpIndex, setActiveOtpIndex] = useState<number>(0);
+  const [otp, setOtp] = useState("");
+  const onChange = (value: string) => setOtp(value);
 
-  const indexRef = useRef<HTMLInputElement>(null);
-
-  function handleChange({ target }: React.ChangeEvent<HTMLInputElement>) {
-    const { value } = target;
-    const newOtp: string[] = [...otp];
-    newOtp[currentOtpIndex] = value.substring(value.length - 1);
-
-    if (!value) {
-      setActiveOtpIndex(currentOtpIndex - 1);
-    } else setActiveOtpIndex(currentOtpIndex + 1);
-    setOtp(newOtp);
-  }
-
-  function handlekeyDown(
-    { key }: React.KeyboardEvent<HTMLInputElement>,
-    index: number
-  ) {
-    currentOtpIndex = index;
-    if (key == "Backspace") setActiveOtpIndex(currentOtpIndex - 1);
-  }
-
-  useEffect(() => {
-    indexRef.current?.focus();
-  }, [activeOtpIndex]);
+  const otpLen = useMemo(
+    () =>
+      otp
+        .trim()
+        .split("")
+        .filter((str) => str != " ")
+        .join("").length,
+    [otp]
+  );
 
   return (
     <section className="relative flex min-h-[500px] w-full items-center justify-center pt-16">
@@ -40,36 +24,29 @@ export default function Otp() {
         <ThemeSwitch />
       </div>
 
-      <div className="mt-8 w-[98%] max-w-[450px] rounded-md bg-gray-900 p-7 text-gray-100 dark:bg-gray-100 dark:text-gray-800">
+      <div className="mt-8 w-[98%] max-w-[430px] rounded-md bg-gray-900 p-7 text-gray-100 dark:bg-gray-100 dark:text-gray-800">
         <div>
           <header className="flex flex-col gap-3 text-center font-semibold">
             <h2 className="text-3xl">Verify Your Account</h2>
-            <p className="flex flex-col gap-1 text-sm text-gray-300 dark:text-gray-700">
+            <p className="flex flex-col gap-1 text-sm text-gray-400 dark:text-gray-600">
+              <span>Eight digits code has been sent to your email.</span>
               <span>
-                We emailed{" "}
-                <span className="text-gray-500">mmejuenoch@gmail.com</span> a
-                six digit code
+                Enter / Copy & paste the code into the inputs below to verify
+                your account.
               </span>
-              <span>Enter the code below to confirm your email address</span>
             </p>
           </header>
 
-          <div className="mt-8 flex w-full flex-wrap items-center justify-center gap-3">
-            {otp.map((_, index) => (
-              <input
-                ref={index === activeOtpIndex ? indexRef : null}
-                key={index}
-                type="number"
-                className="remove-default h-[60px] w-[60px] rounded-md bg-gray-100 text-center text-4xl font-semibold text-gray-800 outline-none dark:bg-gray-800 dark:text-gray-100"
-                onChange={handleChange}
-                onKeyDown={(e) => handlekeyDown(e, index)}
-                value={otp[index]}
-              />
-            ))}
-          </div>
+          <OtpInput {...{ value: otp, valueLength: 8, onChange }} />
 
           <div className="mt-8 h-14 w-full">
-            <button className="h-full w-full rounded-md bg-maingreen-200 text-gray-800 hover:opacity-95 dark:border dark:border-gray-800">
+            <button
+              className={`h-full w-full rounded-md ${
+                otpLen === 8
+                  ? "bg-maingreen-200"
+                  : "cursor-not-allowed bg-gray-500"
+              } text-gray-800 hover:opacity-95 dark:border dark:border-gray-800`}
+            >
               Verify
             </button>
           </div>
