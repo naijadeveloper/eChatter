c8be1cee40a8b01ce6dec1667188fa7e0d659841
Enoch <mmejuenoch@gmail.com>
2023-06-17 21:41:51 +0100

    Almost done with the aside section in the create page. Couldn't find inspiration and motivation today. Let's see what tomorrow brings.

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -2,7 +2,8 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 
 import { HiOutlineArrowNarrowLeft } from "react-icons/hi";
-import { MdArrowDropDown, MdArrowDropUp } from "react-icons/md";
+import { MdArrowDropDown, MdArrowDropUp, MdClose } from "react-icons/md";
+import { IoIosArrowForward } from "react-icons/io";
 
 import LoadingSpinner from "@/components/LoadingSpinner";
 
@@ -10,6 +11,19 @@ export default function createPage() {
   const router = useRouter();
 
   const [contributeDropDown, setContributeDropDown] = useState<boolean>(false);
+
+  const enum visibilityOptions {
+    ALL = "Visible to everyone on the internet",
+    PRIV1 = "Visible to only eChatters",
+    PRIV2 = "Visible to only eChatters following me",
+  }
+  const [visiblityChoice, setVisibilityChoice] = useState<string>(
+    visibilityOptions.ALL
+  );
+
+  function handleVisibilitySetup(choice: string) {
+    setVisibilityChoice(choice);
+  }
 
   return (
     <>
@@ -41,75 +55,192 @@ export default function createPage() {
         className="flex min-h-[500px] py-5"
       >
         {/* Aside bar for eChat options */}
-        <aside className="min-h-[100px] w-[30%] border-r border-gray-500 px-4">
-          <div className="border-b border-gray-500 pb-2">
-            <h2 className="text-center text-xl">Create a new eChat</h2>
-            <p className="text-gray-700 dark:text-gray-300">
-              You have a drafted eChat and would like to continue editing?
-            </p>
-            <button className="text-maingreen-300 underline-offset-2 hover:underline dark:text-maingreen-100">
-              Import draft from storage
-            </button>
-          </div>
+        <aside className="relative max-h-[536px] min-h-[100px] w-[30%] overflow-y-auto overflow-x-hidden">
+          <div className="px-3">
+            <div className="border-b border-gray-500 pb-2">
+              <h2 className="text-center text-xl">Create a new eChat</h2>
+              <p className="text-gray-700 dark:text-gray-300">
+                You have a drafted eChat and would like to continue editing?
+              </p>
+              <button className="text-maingreen-300 underline-offset-2 hover:underline dark:text-maingreen-100">
+                Import draft from storage
+              </button>
+            </div>
 
-          <div>
-            <p className="text-sm italic text-gray-700 dark:text-gray-300">
-              Required fields are marked with an asterisk(*)
-            </p>
+            <div>
+              <p className="text-sm italic text-gray-700 dark:text-gray-300">
+                Important fields are marked with an asterisk(*)
+              </p>
 
-            <div className="mt-2 flex items-center justify-between">
-              <div className="z-[1] flex max-w-[50%] flex-col overflow-visible pb-2">
-                <p>Owner</p>
-                <p className="max-w-[200px] truncate rounded bg-maingreen-300 p-1 px-2 text-sm font-semibold text-gray-100 hover:w-fit hover:max-w-[400px] dark:bg-maingreen-200 dark:text-gray-800">
-                  mmejuenoch-gmail
+              <div className="mt-2 flex items-center justify-between">
+                <div className="z-[1] flex max-w-[50%] flex-col overflow-visible pb-2">
+                  <p>Owner</p>
+                  <p className="max-w-[200px] truncate rounded bg-maingreen-300 p-1 px-2 text-sm font-semibold text-gray-100 hover:w-fit hover:max-w-[400px] dark:bg-maingreen-200 dark:text-gray-800">
+                    mmejuenoch-gmail
+                  </p>
+                </div>
+
+                <div className="relative flex flex-col pb-2">
+                  <p>Contributors</p>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setContributeDropDown(!contributeDropDown);
+                    }}
+                    className="flex items-center justify-between rounded bg-gray-400/80 p-1 px-2 text-sm font-semibold hover:bg-gray-400 dark:bg-gray-700 hover:dark:bg-gray-700/80"
+                  >
+                    <span>0 out of 10 chosen</span>
+                    {contributeDropDown ? (
+                      <MdArrowDropUp />
+                    ) : (
+                      <MdArrowDropDown />
+                    )}
+                  </button>
+
+                  {contributeDropDown && (
+                    <div
+                      onClick={(e) => e.stopPropagation()}
+                      className="absolute -right-2 top-12 z-[2] flex min-h-[120px] w-72 items-center justify-center rounded bg-gray-300 p-2 drop-shadow-[0px_1px_2px_rgb(54,_54,_54)] dark:bg-gray-800 dark:drop-shadow-[0px_1px_2px_#030712]"
+                    >
+                      {/* <LoadingSpinner position="absolute" size="text-6xl" /> */}
+                      <span className="text-gray-500">coming soon</span>
+                    </div>
+                  )}
+                </div>
+              </div>
+
+              <div className="mt-2">
+                <p>eChat title*</p>
+                <textarea
+                  maxLength={100}
+                  className="min-h-[100px] w-full resize-none rounded border-2 border-transparent bg-gray-300 px-2 text-lg focus:border-maingreen-300/30 focus:outline-none dark:bg-gray-800 dark:focus:border-maingreen-100/30"
+                ></textarea>
+                <p className="flex flex-col text-sm text-gray-700 dark:text-gray-300">
+                  <span>
+                    eChat titles can't be more than 100 characters long. So make
+                    it short and memorable.
+                  </span>
+                  <span>
+                    e.g "Unveiling the secrets: The enigma of Human Existence."
+                  </span>
+                </p>
+
+                <button className="mt-2 flex items-center rounded bg-gray-400/80 p-1 text-sm font-semibold hover:bg-gray-400 dark:bg-gray-700 hover:dark:bg-gray-700/80">
+                  <span>Add cover image</span>
+                </button>
+                <p className="flex flex-wrap items-center gap-1 text-sm text-gray-700 dark:text-gray-300">
+                  <span className="truncate">image_of_a_wanted_man.jpg</span>
+                  <button className="text-base">
+                    <MdClose />
+                  </button>
                 </p>
               </div>
 
-              <div
-                onClick={(e) => e.stopPropagation()}
-                className="relative flex flex-col pb-2"
-              >
-                <p>Contributors</p>
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    setContributeDropDown(!contributeDropDown);
-                  }}
-                  className="flex items-center justify-between rounded bg-gray-400 p-1 px-2 text-sm font-semibold dark:bg-gray-700"
-                >
-                  <span>0 out of 10 chosen</span>
-                  {contributeDropDown ? <MdArrowDropUp /> : <MdArrowDropDown />}
-                </button>
+              <div className="mt-4 border-t border-gray-500 py-2">
+                <p>Visibility Setting*</p>
 
-                {contributeDropDown && (
-                  <div className="absolute right-0 top-12 z-[2] flex min-h-[120px] w-72 items-center justify-center rounded bg-gray-300 p-2 drop-shadow-[0px_1px_2px_rgb(54,_54,_54)] dark:bg-gray-800 dark:drop-shadow-[0px_1px_2px_#030712]">
+                <div
+                  onClick={() => handleVisibilitySetup(visibilityOptions.ALL)}
+                  className={`group mt-2 flex cursor-pointer items-center gap-1 p-1 px-2 ${
+                    visiblityChoice == visibilityOptions.ALL &&
+                    "text-maingreen-300 dark:text-maingreen-100"
+                  }`}
+                >
+                  {visiblityChoice == visibilityOptions.ALL && (
+                    <span className="text-xl">
+                      <IoIosArrowForward />
+                    </span>
+                  )}
+                  <p
+                    className={`text-sm group-hover:underline group-hover:underline-offset-2 ${
+                      visiblityChoice == visibilityOptions.ALL &&
+                      "underline underline-offset-2"
+                    }`}
+                  >
+                    Visible to everyone on the internet i.e both registered and
+                    non-registered users
+                  </p>
+                </div>
+
+                <div
+                  onClick={() => handleVisibilitySetup(visibilityOptions.PRIV1)}
+                  className={`group mt-2 flex cursor-pointer items-center gap-1 p-1 px-2 ${
+                    visiblityChoice == visibilityOptions.PRIV1 &&
+                    "text-maingreen-300 dark:text-maingreen-100"
+                  }`}
+                >
+                  {visiblityChoice == visibilityOptions.PRIV1 && (
+                    <span className="text-xl">
+                      <IoIosArrowForward />
+                    </span>
+                  )}
+                  <p
+                    className={`text-sm group-hover:underline group-hover:underline-offset-2 ${
+                      visiblityChoice == visibilityOptions.PRIV1 &&
+                      "underline underline-offset-2"
+                    }`}
+                  >
+                    Visible to only eChatters i.e only registered users
+                  </p>
+                </div>
+
+                <div
+                  onClick={() => handleVisibilitySetup(visibilityOptions.PRIV2)}
+                  className={`group mt-2 flex cursor-pointer items-center gap-1 p-1 px-2 ${
+                    visiblityChoice == visibilityOptions.PRIV2 &&
+                    "text-maingreen-300 dark:text-maingreen-100"
+                  }`}
+                >
+                  {visiblityChoice == visibilityOptions.PRIV2 && (
+                    <span className="text-xl">
+                      <IoIosArrowForward />
+                    </span>
+                  )}
+                  <p
+                    className={`text-sm group-hover:underline group-hover:underline-offset-2 ${
+                      visiblityChoice == visibilityOptions.PRIV2 &&
+                      "underline underline-offset-2"
+                    }`}
+                  >
+                    Visible to only eChatters following me
+                  </p>
+                </div>
+              </div>
+              {/* end of visibility settings div */}
+
+              <div className="mt-4 border-t border-gray-500 py-2">
+                <p className="text-center">
+                  Select the category this eChat belongs to*
+                </p>
+                <div className="relative mt-2 flex flex-col items-center">
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                    }}
+                    className="flex w-[70%] items-center justify-evenly rounded bg-gray-400/80 p-1 px-2 text-sm font-semibold hover:bg-gray-400 dark:bg-gray-700 hover:dark:bg-gray-700/80"
+                  >
+                    <span>News</span>
+                  </button>
+
+                  <div
+                    onClick={(e) => e.stopPropagation()}
+                    className="absolute bottom-6 right-8 z-[2] flex min-h-[120px] w-72 items-center justify-center rounded bg-gray-300 p-2 drop-shadow-[0px_1px_2px_rgb(54,_54,_54)] dark:bg-gray-800 dark:drop-shadow-[0px_1px_2px_#030712]"
+                  >
                     {/* <LoadingSpinner position="absolute" size="text-6xl" /> */}
                     <span className="text-gray-500">coming soon</span>
                   </div>
-                )}
+                </div>
               </div>
+              {/* end of collections div */}
             </div>
-
-            <div className="mt-2">
-              <p>eChat title*</p>
-              <textarea
-                maxLength={100}
-                className="min-h-[100px] w-full resize-none rounded border-2 border-transparent bg-gray-300 px-2 text-lg focus:border-maingreen-300/30 focus:outline-none dark:bg-gray-800 dark:focus:border-maingreen-100/30"
-              ></textarea>
-              <p className="flex flex-col text-sm text-gray-700 dark:text-gray-300">
-                <span>
-                  eChat names can't be more than 100 characters long. So make it
-                  short and memorable.
-                </span>
-                <span>
-                  e.g "Unveiling the secrets: The enigma of Human Existence."
-                </span>
-              </p>
-
-              <button className="mt-2 flex items-center rounded bg-gray-400 p-1 px-2 text-sm font-semibold dark:bg-gray-700">
-                <span>Add cover image</span>
-              </button>
+          </div>
+          <div className="sticky bottom-0 left-0 z-[3] mt-2 flex w-full items-center justify-center gap-6 border-gray-500 bg-gray-300 py-2 dark:bg-gray-800">
+            <div className="relative flex w-[90px] cursor-pointer items-center justify-center rounded bg-maingreen-300 p-1 px-3 font-semibold text-gray-100 dark:bg-maingreen-200 dark:text-gray-800">
+              <p>Post</p>
             </div>
+            <button className="hover:underline hover:underline-offset-2">
+              Save as draft
+            </button>
           </div>
         </aside>
 
