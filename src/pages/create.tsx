9ffc576dfe2d7973b6686e8644c21adc19fc5562
Enoch <mmejuenoch@gmail.com>
2023-06-19 22:24:33 +0100

    Almost done with the tags

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -7,8 +7,13 @@ import {
   MdArrowDropUp,
   MdClose,
   MdCheck,
+  MdVisibility,
 } from "react-icons/md";
 import { IoIosArrowForward } from "react-icons/io";
+import { FaUsers, FaUser } from "react-icons/fa";
+import { BsFillChatSquareFill, BsImage } from "react-icons/bs";
+import { AiFillTags } from "react-icons/ai";
+import { BiCategory } from "react-icons/bi";
 
 import LoadingSpinner from "@/components/LoadingSpinner";
 
@@ -21,6 +26,28 @@ export default function createPage() {
     PRIV2 = "Visible to only eChatters following me",
   }
 
+  const categories = [
+    "News",
+    "Sports",
+    "Music",
+    "Movie",
+    "Science",
+    "Art",
+    "Fashion",
+    "Health",
+    "Lifestyle",
+    "Travel",
+    "Food & Cooking",
+    "Business & Finance",
+    "Education",
+    "Politics",
+    "Environment",
+    "Relationship",
+    "Personal Development",
+    "DIY & Crafts",
+    "Religion",
+  ];
+
   const [contributeDropDown, setContributeDropDown] = useState<boolean>(false);
   const [visiblityChoice, setVisibilityChoice] = useState<string>(
     visibilityOptions.ALL
@@ -28,7 +55,7 @@ export default function createPage() {
 
   const [openCategoryDropDown, setOpenCategoryDropDown] =
     useState<boolean>(false);
-  const [category, setCategory] = useState<string>("News");
+  const [category, setCategory] = useState<string>(categories[0]);
 
   function handleVisibilitySetup(choice: string) {
     setVisibilityChoice(choice);
@@ -37,7 +64,10 @@ export default function createPage() {
   return (
     <>
       <header
-        onClick={() => setContributeDropDown(false)}
+        onClick={() => {
+          setContributeDropDown(false);
+          setOpenCategoryDropDown(false);
+        }}
         className="sticky top-0 z-20 mx-auto flex items-center bg-gray-100 px-2 py-4 dark:bg-gray-900"
       >
         <div className="flex w-full">
@@ -60,7 +90,10 @@ export default function createPage() {
       </header>
 
       <section
-        onClick={() => setContributeDropDown(false)}
+        onClick={() => {
+          setContributeDropDown(false);
+          setOpenCategoryDropDown(false);
+        }}
         className="flex min-h-[500px] py-5"
       >
         {/* Aside bar for eChat options */}
@@ -83,14 +116,24 @@ export default function createPage() {
 
               <div className="mt-2 flex items-center justify-between">
                 <div className="z-[1] flex max-w-[50%] flex-col overflow-visible pb-2">
-                  <p>Owner</p>
+                  <p className="flex items-center gap-1">
+                    <span>
+                      <FaUser />
+                    </span>
+                    <span>Owner</span>
+                  </p>
                   <p className="max-w-[200px] truncate rounded bg-maingreen-300 p-1 px-2 text-sm font-semibold text-gray-100 hover:w-fit hover:max-w-[400px] dark:bg-maingreen-200 dark:text-gray-800">
                     mmejuenoch-gmail
                   </p>
                 </div>
 
                 <div className="relative flex flex-col pb-2">
-                  <p>Contributors</p>
+                  <p className="flex items-center gap-1">
+                    <span>
+                      <FaUsers />
+                    </span>
+                    <span>Contributors</span>
+                  </p>
                   <button
                     onClick={(e) => {
                       e.stopPropagation();
@@ -119,22 +162,30 @@ export default function createPage() {
               </div>
 
               <div className="mt-2">
-                <p>eChat title*</p>
+                <p className="flex items-center gap-1">
+                  <span>
+                    <BsFillChatSquareFill />
+                  </span>
+                  <span>eChat Title*</span>
+                </p>
                 <textarea
                   maxLength={100}
                   className="min-h-[100px] w-full resize-none rounded border-2 border-transparent bg-gray-300 px-2 text-lg focus:border-maingreen-300/30 focus:outline-none dark:bg-gray-800 dark:focus:border-maingreen-100/30"
                 ></textarea>
                 <p className="flex flex-col text-sm text-gray-700 dark:text-gray-300">
                   <span>
-                    eChat titles can't be more than 100 characters long. So make
-                    it short and memorable.
+                    Note: eChat titles can't be more than 100 characters long.
+                    So make it short and memorable.
                   </span>
                   <span>
                     e.g "Unveiling the secrets: The enigma of Human Existence."
                   </span>
                 </p>
 
-                <button className="mt-2 flex items-center rounded bg-gray-400/80 p-1 text-sm font-semibold hover:bg-gray-400 dark:bg-gray-700 hover:dark:bg-gray-700/80">
+                <button className="mt-2 flex items-center gap-1 rounded bg-gray-400/80 p-1 text-sm font-semibold hover:bg-gray-400 dark:bg-gray-700 hover:dark:bg-gray-700/80">
+                  <span>
+                    <BsImage />
+                  </span>
                   <span>Add cover image</span>
                 </button>
                 <p className="flex flex-wrap items-center gap-1 text-sm text-gray-700 dark:text-gray-300">
@@ -146,7 +197,100 @@ export default function createPage() {
               </div>
 
               <div className="mt-4 border-t border-gray-500 py-2">
-                <p>Visibility Setting*</p>
+                <div>
+                  <p className="flex items-center gap-1">
+                    <span>
+                      <AiFillTags />
+                    </span>
+                    <span>Tags*</span>
+                  </p>
+                  <p>
+                    Press enter or add a comma after each tag then press enter
+                  </p>
+
+                  <div>
+                    <ul className="flex border-2 border-maingreen-300/30 p-2 dark:border-maingreen-100/30">
+                      <li className="m-1 flex items-center gap-2 rounded bg-gray-400/50 p-1 px-2 dark:bg-gray-700/50">
+                        <span>html</span>
+                        <button>
+                          <MdClose />
+                        </button>
+                      </li>
+                      <li className="m-1 flex items-center gap-2 rounded bg-gray-400/50 p-1 px-2 dark:bg-gray-700/50">
+                        <span>css</span>
+                        <button>
+                          <MdClose />
+                        </button>
+                      </li>
+                      <input
+                        type="text"
+                        className="flex-1 border-0 p-1 outline-none"
+                      />
+                    </ul>
+                  </div>
+
+                  <div className="flex items-center justify-between">
+                    <p>
+                      <span>5</span> tags are remaining
+                    </p>
+                    <button className="rounded bg-gray-400/80 p-1 px-2 hover:bg-gray-400 dark:bg-gray-700 hover:dark:bg-gray-700/80">
+                      Remove all
+                    </button>
+                  </div>
+                </div>
+                {/* tags div */}
+
+                <div className="mt-2">
+                  <p className="flex items-center gap-1">
+                    <span>
+                      <BiCategory />
+                    </span>
+                    <span>Select the category this eChat belongs to*</span>
+                  </p>
+                  <div className="relative mt-2 flex flex-col items-center">
+                    <button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setOpenCategoryDropDown(!openCategoryDropDown);
+                      }}
+                      className="flex w-full items-center justify-evenly rounded bg-gray-400/80 p-1 px-2 font-semibold hover:bg-gray-400 dark:bg-gray-700 hover:dark:bg-gray-700/80"
+                    >
+                      <span>{category}</span>
+                    </button>
+
+                    {openCategoryDropDown && (
+                      <div
+                        onClick={(e) => e.stopPropagation()}
+                        className="dropdown-scroll absolute bottom-6 z-[2] flex max-h-[250px] min-h-[120px] min-w-[260px] flex-col gap-y-[2px] overflow-auto rounded bg-gray-300 p-2 drop-shadow-[0px_1px_2px_rgb(54,_54,_54)] dark:bg-gray-800 dark:drop-shadow-[0px_1px_2px_#030712]"
+                      >
+                        {/* <LoadingSpinner position="absolute" size="text-6xl" /> */}
+                        {categories.map((cat, index) => (
+                          <p
+                            key={index}
+                            onClick={() => setCategory(cat)}
+                            className={`flex cursor-pointer items-center gap-1 rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
+                              category === cat &&
+                              "text-maingreen-300 underline underline-offset-2 dark:text-maingreen-100"
+                            }`}
+                          >
+                            {category === cat && <MdCheck />}
+                            {cat}
+                          </p>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+              {/* end of category and tags div */}
+
+              <div className="mt-4 border-t border-gray-500 py-2">
+                <p className="flex items-center gap-1">
+                  <span>
+                    <MdVisibility />
+                  </span>
+                  <span>Visibility Setting*</span>
+                </p>
 
                 <div
                   onClick={() => handleVisibilitySetup(visibilityOptions.ALL)}
@@ -216,204 +360,6 @@ export default function createPage() {
                 </div>
               </div>
               {/* end of visibility settings div */}
-
-              <div className="mt-4 border-t border-gray-500 py-2">
-                <p className="text-center">
-                  Select the category this eChat belongs to*
-                </p>
-                <div className="relative mt-2 flex flex-col items-center">
-                  <button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                    }}
-                    className="flex w-[70%] items-center justify-evenly rounded bg-gray-400/80 p-1 px-2 text-sm font-semibold hover:bg-gray-400 dark:bg-gray-700 hover:dark:bg-gray-700/80"
-                  >
-                    <span>News</span>
-                  </button>
-
-                  <div
-                    onClick={(e) => e.stopPropagation()}
-                    className="absolute bottom-6 z-[2] flex min-h-[120px] min-w-[100px] flex-col gap-y-[2px] rounded bg-gray-300 p-2 drop-shadow-[0px_1px_2px_rgb(54,_54,_54)] dark:bg-gray-800 dark:drop-shadow-[0px_1px_2px_#030712]"
-                  >
-                    {/* <LoadingSpinner position="absolute" size="text-6xl" /> */}
-                    <p
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        setCategory("News");
-                      }}
-                      className={`flex cursor-pointer items-center gap-1 rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "News" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      {category == "News" && <MdCheck />}
-                      News
-                    </p>
-
-                    <p
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        setCategory("Sports");
-                      }}
-                      className={`flex cursor-pointer items-center gap-1 rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "Sports" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      {category == "News" && <MdCheck />}
-                      News
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "Music" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      Music
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "Movie" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      Movie
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "Science" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      Science
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "Art" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      Art
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "Fashion" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      Fashion
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "Health" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      Health
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "Lifestyle" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      Lifestyle
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "Travel" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      Travel
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "f-n-c" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      Food & Cooking
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "b-n-f" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      Business & Finance
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "Education" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      Education
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "Politics" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      Politics
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 ${
-                        category == "Environment" &&
-                        "bg-maingreen-300 underline underline-offset-2 dark:bg-maingreen-100"
-                      }`}
-                    >
-                      Environment
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700`}
-                    >
-                      Relationship
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700`}
-                    >
-                      Personal Development
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700`}
-                    >
-                      Career
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700`}
-                    >
-                      DIY & Crafts
-                    </p>
-
-                    <p
-                      className={`cursor-pointer rounded p-1 px-2 hover:bg-gray-400 dark:hover:bg-gray-700`}
-                    >
-                      Religion
-                    </p>
-                  </div>
-                </div>
-              </div>
-              {/* end of collections div */}
             </div>
           </div>
           <div className="sticky bottom-0 left-0 z-[3] mt-2 flex w-full items-center justify-center gap-6 border-gray-500 bg-gray-300 py-2 dark:bg-gray-800">
