05f58eea139501f69acf52d2e33484e1b4e7004f
Enoch <mmejuenoch@gmail.com>
2023-07-02 20:15:39 +0100

    Adjustments to Ui of create page.

--- src/components/MdPreview.tsx
+++ src/components/MdPreview.tsx
@@ -43,7 +43,7 @@ export default function MdPreview({
         </div>
 
         {title && (
-          <p className={`mx-auto ${image? "mt-3" : "mt-12"} flex w-[96%] items-center justify-center text-center text-4xl font-bold capitalize underline decoration-gray-500 underline-offset-2`}>
+          <p className={`mx-auto ${image? "mt-3" : "mt-12"} flex w-[96%] items-center justify-center text-center text-5xl max-md:text-4xl font-bold capitalize underline decoration-gray-500 underline-offset-2`}>
             {title}
           </p>
         )}
@@ -56,7 +56,7 @@ export default function MdPreview({
 
         <ReactMarkdown
           children={content}
-          className={`mdstyleAll prose prose-gray ${(image || title || tags.length > 0)? "mt-2" : "mt-10"} min-h-full min-w-full max-w-full rounded p-2 dark:prose-invert md:prose-lg prose-headings:break-words prose-headings:mb-2 prose-headings:mt-5 prose-p:break-words prose-p:my-2 prose-a:text-blue-600 prose-pre:border-2 prose-pre:border-transparent prose-pre:bg-gray-900 dark:prose-a:text-blue-400`}
+          className={`mdstyleAll prose prose-gray ${(image || title || tags.length > 0)? "mt-2" : "mt-10"} min-w-full max-w-full rounded p-2 dark:prose-invert sm:prose-lg prose-headings:break-words prose-headings:mb-2 prose-headings:mt-5 prose-p:break-words prose-p:my-2 prose-a:text-blue-600 prose-pre:border-2 prose-pre:border-transparent prose-pre:bg-gray-900 dark:prose-a:text-blue-400`}
         />
 
         {category && (<div className={`absolute ${image? "top-4 right-4" : "top-2 right-2"} bg-maingreen-300 dark:bg-maingreen-200 p-2 min-w-[70px] flex items-center justify-center rounded border border-gray-800 text-gray-100 dark:text-gray-800 uppercase font-semibold`}>
