0d1e584b84b44120446c658c016046a5a2b27217
Enoch <mmejuenoch@gmail.com>
2023-07-08 21:51:26 +0100

    Learning and implementing mongodb using the mongoose driver

--- src/database/databaseModels.ts
+++ src/database/databaseModels.ts
@@ -1,15 +1,37 @@
-import mongoose, { InferSchemaType, model, Schema} from "mongoose";
+import { InferSchemaType, model, Schema} from "mongoose";
 
-// User schema
+// User schema and model
 const userSchema = new Schema({
-  username: String,
-  fullname: String,
-  email: String,
-  password: String,
-  verified: Boolean,
-  otp: Number,
+  fullname: {
+    firstname: String,
+    lastname: String,
+  },
+  username: {type: String, required: true, lowercase: true},
+  email: {type: String, required: true, lowercase: true},
+  password: {type: String, required: true},
+  verified: {type: Boolean, default: false},
+  image: Buffer,
+  dateOfBirth: Date,
+  bio: String,
+  following: [{type: Schema.Types.ObjectId, ref: "users"}],
+  followers: [{type: Schema.Types.ObjectId, ref: "users"}],
+  theme: {
+    type: String,
+    default: "dark"
+  },
+  createdAt: {type: Date, default: Date.now()}
 });
 
-type users = InferSchemaType<typeof userSchema>
-
+type users = InferSchemaType<typeof userSchema>;
 export const usersCollection = model<users>("users", userSchema);
+
+
+//otp schema and model
+const otpSchema = new Schema({
+  userId: {type: String, required: true},
+  otpCode: {type: String, required: true},
+  createdAt: {type: Date, default: Date.now()}
+});
+
+type otpVerification = InferSchemaType<typeof otpSchema>;
+export const otpCollection = model<otpVerification>("otpVerification", otpSchema);
--- src/pages/api/users/create-user.ts
+++ src/pages/api/users/create-user.ts
@@ -4,14 +4,38 @@ import { usersCollection } from "@/database/databaseModels";
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  connectMongo()
+  connectMongo().catch(() => res.status(500).end("Server error"))
   
   // check method
   if(req.method === "POST") {
-    //insert new user to users collection
+    const { email, username, password } = req.body;
+    if(!email || !username || !password) {
+      return res.status(400).json({error: "Empty field"})
+    }
 
+    // check if user already exist
+    const user = await usersCollection.find({email}).count();
+    if(!user) return res.status(404).json({error: "User already exist"});
+
+    // insert new user to users collection
+    const createdUser = await usersCollection.create({
+      email,
+      username,
+      password
+    })
+    try{
+      await createdUser.save();
+      // run function to generate otp and save in otp collection
+      otpGeneration(createdUser._id.toString())
+    }catch(error) {return res.status(500).json({error: "Couldn't save"})}
   }else {
     res.setHeader("Allow", ["POST"]);
     res.status(405).end("method not allowed");
   }
+}
+
+async function otpGeneration(_id: string) {
+  // generate random 8 char number and encrypt it
+  // create otp document and save the _id of user created and the generated otp code
+  //use nodemailer to send to user the email
 }
