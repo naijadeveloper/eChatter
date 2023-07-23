15be51600cc59ec339036213657398e22774ee81
Enoch <mmejuenoch@gmail.com>
2023-07-23 22:03:23 +0100

    ...

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials"
 
 export default NextAuth({
   providers: [
