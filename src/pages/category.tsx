a229cf3e5964db677b76d46f71c645a265116bbc
Enoch <mmejuenoch@gmail.com>
2023-09-01 23:52:26 +0100

    ...

--- src/pages/category.tsx
+++ src/pages/category.tsx
@@ -5,7 +5,9 @@ import Footer from "@/components/Footer";
 export default function categorySelectingPage() {
   return (
     <>
-      <section></section>
+      <section className="min-h-[500px] px-2 py-5">
+        Hello
+      </section>
       <Footer />
     </>
   );
